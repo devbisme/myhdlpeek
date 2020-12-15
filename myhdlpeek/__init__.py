ba68c0c6bbb25ee3ea996fd756867cc1c5bcf3f9
xesscorp <devb@xess.com>
2020-12-15 17:13:35 -0500

    Fixed drawing of traces.

--- myhdlpeek/__init__.py
+++ myhdlpeek/__init__.py
@@ -3,3 +3,7 @@
 # Copyright (c) 2017-2020, XESS Corp. The MIT License (MIT).
 
 from .pckg_info import version
+
+# If you just import top-level myhdlpeek, then you'll get the MyHDL version
+# of the Peeker. 
+from .myhdl import *
